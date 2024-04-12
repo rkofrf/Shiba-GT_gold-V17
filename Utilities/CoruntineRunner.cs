afdd5981ebbe5ea1355e805a1d83ad0f9259cec7
rkofrf <159555490+rkofrf@users.noreply.github.com>
2024-04-12 11:51:49 -0400

    Add files via upload

---
+++ Utilities/CoruntineRunner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Mono;
+using System.Collections;
+
+public class CoroutineRunner : MonoBehaviour
+{
+    private static CoroutineRunner instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public static void RunCoroutine(IEnumerator coroutine)
+    {
+        instance.StartCoroutine(coroutine);
+    }
+
+    public static void StopRunningCoroutine(IEnumerator coroutine)
+    {
+        instance.StopCoroutine(coroutine);
+    }
+}
